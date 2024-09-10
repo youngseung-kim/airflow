7e7d6c16900a92c171ff7fa09d9e410955331ee5
youngseung-kim <davinciofch@gamail.com>
2024-09-10 17:39:16 +0900

    conn test2

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_conn_test",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False
