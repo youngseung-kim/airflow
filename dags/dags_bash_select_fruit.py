6abb81417ee612b381a167b22f9eecde6fab4089
youngseung-kim <davinciofch@gamail.com>
2024-09-11 09:48:48 +0900

    shell test

---
+++ dags/dags_bash_select_fruit.py
@@ -0,0 +1,26 @@
+import datetime
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+
+ t1_orange = BashOperator(
+        task_id="t1_orange",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
+    )
+ 
+ t2_avocado = BashOperator(
+        task_id="t2_avocado",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
+    )
+
+  t1_orange >> t2_avocado
+
+ 
